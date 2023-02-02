f11713ed4862a6ab11028813351fbd56e4a45fef
Nikki NoLife <nikkinolife@gmail.com>
2023-02-02 09:42:41 +0400

    feat: add test-data and test starting page, add redux, add scss support

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TestMainPage from './pages/TestMainPage/TestMainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestMainPage />
     </div>
   );
 }
---
+++ src/data/test-data/User.ts
@@ -0,0 +1,15 @@
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  nickName: string;
+  location: string;
+  country: string;
+  city: string;
+  avatarImg: string;
+  likeCats: boolean;
+  likeDogs: boolean;
+  favoriteFilm: string;
+}
+
+export default User;
