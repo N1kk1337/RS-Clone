469a780b216c71b8b7185dfe647569d6eb2a5e86
KarinaBertosh <cherrycake246@gmail.com>
2023-02-06 15:54:04 +0300

    feat: add user information

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Button } from 'react-bootstrap';
 // import { useNavigate } from 'react-router-dom';
+import data from '../../data/test-data/test-data.json';
 import './style.scss';
 
 function User(): JSX.Element {
@@ -8,12 +9,33 @@ function User(): JSX.Element {
 
   return (
     <div className="user">
-      Информация обо мне:
-      <li>Имя: Karina</li>
-      <li>Описание: Учусь программированию в школе RS School</li>
-      <li>Интересы: велоспорт, настольный теннис</li>
-      <li>Возраст: 22</li>
-      <li>Гео: Беларусь</li>
+      <img className="avatar" src={data.products[0].avatarImg} alt="avatar" />
+      <li>
+        First name and Last name:&nbsp;
+        {data.products[0].firstName}
+        &nbsp;
+        {data.products[0].lastName}
+      </li>
+      <li>
+        Location:&nbsp;
+        {data.products[0].location}
+      </li>
+      <li>
+        Country:&nbsp;
+        {data.products[0].country}
+      </li>
+      <li>
+        City:&nbsp;
+        {data.products[0].city}
+      </li>
+      <li>
+        Like cats:&nbsp;
+        {data.products[0].likeCats}
+      </li>
+      <li>
+        Like dogs:&nbsp;
+        {data.products[0].likeDogs}
+      </li>
     </div>
   );
 }
