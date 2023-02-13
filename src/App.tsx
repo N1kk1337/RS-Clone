fd94a65925f64e4dae54898241c0e0848cbd5008
Baxromxoja16 <aglamof@mail.ru>
2023-02-13 21:27:28 +0500

    fix: app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Header from './components/Header/Header';
 import './App.css';
 import Footer from './components/Footer/Footer';
-import FeedPost from './pages/FeedPost/FeedPost';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <FeedPost />
       <Footer />
     </div>
   );
