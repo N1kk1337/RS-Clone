e696c4cc06c91a58ff9cacfd06d18fb060488651
Nikki NoLife <nikkinolife@gmail.com>
2023-02-14 18:58:36 +0400

    fix: root styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  Navigate,
-  Route, Routes,
-} from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/Landing/LandingPage';
 import UserPage from './pages/UserPage/UserPage';
-import './App.css';
+import './App.scss';
 
 export const emptyPath = '/';
 export const userPage = '/user-page';
@@ -17,11 +16,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path={`${emptyPath}`} element={<LandingPage />} />
-        <Route path={`${userPage}`} element={<UserPage />} />
-        <Route path={`${starPath}`} element={<Navigate to="" />} />
-      </Routes>
+      <Container className="content">
+        <Routes>
+          <Route path={`${emptyPath}`} element={<LandingPage />} />
+          <Route path={`${userPage}`} element={<UserPage />} />
+          <Route path={`${starPath}`} element={<Navigate to="" />} />
+        </Routes>
+      </Container>
       <Footer />
     </div>
   );
