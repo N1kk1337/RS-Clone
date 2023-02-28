7bf676fd4388db6ea7a23544d46294218c333f81
KarinaBertosh <cherrycake246@gmail.com>
2023-02-28 03:04:08 +0300

    fix: layout

--- src/pages/Landing/LandingPage.tsx
+++ src/pages/Landing/LandingPage.tsx
@@ -1,16 +1,12 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react';
 import LoginModal from '../../components/LoginModal/LoginModal';
 import BasicExample from '../../components/RegisterModal/RegisterModal';
+import lending from '../../assets/lending.png';
 import './Landing.scss';
 
 function LandingPage() {
   const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
-  // const { id } = useAppSelector((state) => state.userAuth);
-  // const navigate = useNavigate();
-  // if (id) navigate('/feed');
 
   return (
     <main>
@@ -34,13 +30,13 @@ function LandingPage() {
         <div className="container">
           <div className="row gap-4">
             <div className="index-page-content col-lg-6 col-md-12 text-center">
-              <h2 className="main-title">VK for mobile devices</h2>
+              <h2 className="main-title">TAILS AND PAWS</h2>
               <p className="main-info">
-                Install our official mobile app and stay in touch with your
-                friends anytime and anywhere.
+                This is a social network for those who have pets.
               </p>
               <img
-                src="https://sun7-9.userapi.com/GKUrK0PzHqPH_9lv1fx01p_LO6PjXjMMRxBGJw/VD3VpchXcC8.png"
+                className="lending-img"
+                src={lending}
                 alt=""
               />
             </div>
