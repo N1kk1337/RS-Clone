cc7179dc5e38d0ab705a00a9e3121c2f24d7941e
Nikki NoLife <nikkinolife@gmail.com>
2023-02-28 10:56:25 +0400

    fix: tests

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,46 +1,42 @@
-import { render, screen } from "@testing-library/react";
-import Footer from "./Footer";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
 
-
-test("render footer", () => {
+test('render footer', () => {
   render(<Footer />);
 });
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: () => (jest.fn())
+  useNavigate: () => (jest.fn()),
 }));
 
-test("render id", () => {
+test('render id', () => {
   render(<Footer />);
   const divText = screen.getByTestId('footer');
   expect(divText).toBeInTheDocument();
 });
 
-test("render button", () => {
+test('render button', () => {
   render(<Footer />);
   const button = screen.getByRole('button');
   expect(button).toBeInTheDocument();
 });
 
-test("render linkGitHubNikita", () => {
+test('render linkGitHubNikita', () => {
   render(<Footer />);
   const linkGitHubNikita = screen.getByText('Nikita');
   expect(linkGitHubNikita).toBeInTheDocument();
 });
 
-test("render linkGitHubBaxrom", () => {
+test('render linkGitHubBaxrom', () => {
   render(<Footer />);
   const linkGitHubBaxrom = screen.getByText('Baxrom');
   expect(linkGitHubBaxrom).toBeInTheDocument();
 });
 
-test("render linkGitHubKarina", () => {
+test('render linkGitHubKarina', () => {
   render(<Footer />);
   const linkGitHubKarina = screen.getByText('Karina');
   expect(linkGitHubKarina).toBeInTheDocument();
 });
-
-
-
-
